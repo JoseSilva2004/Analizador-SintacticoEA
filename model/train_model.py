8f1a256ad583420495c310c5ee17ad85af428fc6
isaachernandez24 <isaacalejandrohf@gmail.com>
2025-03-09 23:42:42 -0400

    modelo mejorado y csv

--- model/train_model.py
+++ model/train_model.py
@@ -1,6 +1,6 @@
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 
-def train_model(model, X, y, epochs=100, batch_size=32, validation_split=0.2):
+def train_model(model, X, y, epochs=100, batch_size=32, validation_split=0.05):
     # Callbacks
     early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
     model_checkpoint = ModelCheckpoint('best_model.h5', monitor='val_loss', save_best_only=True)
